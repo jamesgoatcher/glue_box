bb34e40728c5e01537084c863354ace847e17f75
James Goatcher <jamesgoatcher@gmail.com>
2016-08-11 21:56:06 -0700

    All RESTful routes are working for Glues

--- glue_box/app/controllers/glues_controller.rb
+++ glue_box/app/controllers/glues_controller.rb
@@ -12,7 +12,8 @@ class GluesController < ApplicationController
 			puts "======================================"
 		end
 
-		@test = "test test testing"
+		@test  = "test test testing"
+		@glues = Glue.all
 
 	end
 
@@ -22,24 +23,48 @@ class GluesController < ApplicationController
 
 	end
 
+	def new
+
+		@glue = Glue.new
+
+	end
+
 	def create
 
-		@glue = Post.new(glue_params)
+		@glue = Glue.new(glues_params)
 
 		if @glue.save
 			flash[:notice] = "Glued!"
-			redirect_to @glue
+			redirect_to '/glues'
 		else
 			flash[:notice] = "Invalid..."
-			redirect_to root
+			redirect_to 'new_glue_url'
 		end
 
 	end
 
+	def edit
+		@glue = Glue.find(params[:id])
+	end
+
+	def update
+		glue = Glue.find(params[:id])
+		glue.update(glues_params)
+
+		redirect_to '/glues'
+	end
+
+	def destroy
+		glue = Glue.find(params[:id])
+		glue.destroy
+
+		redirect_to '/glues'
+	end
+
 	private
 
-		def glue_params
+		def glues_params
 			params.require(:glue).permit(:title, :user, :content)
 		end
-		
+
 end
